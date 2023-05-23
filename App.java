e40a1d68fa20e15f08199be2024663ef5d0d7b17
coutinhogabriel <gabriel.coutin@hotmail.com>
2023-05-22 21:53:28 -0300

    22/05/23

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ package AtividadeFormativa;
 public class App {
     public static void main(String[] args) {
         Formativa obj = new Formativa();
-        obj.exercicio02();
+        obj.exercicio03();
     }
     
 }
--- Formativa.java
+++ Formativa.java
@@ -7,13 +7,16 @@ public class Formativa {
     Scanner sc = new Scanner(System.in);
     Random rd = new Random();
 
+    // criar matriz com colunas e linhas (ambas informações dadas pelo usuario)
     public void exercicio01() {
+        // solicitar a quantidade de colunas e linhas do usuario
         System.out.println("Digite o número de linhas da sua matriz: ");
         int linha = sc.nextInt();
         System.out.println("Digite o número de colunas da sua matriz: ");
         int coluna = sc.nextInt();
+        // criação da matriz
         int matriz[][] = new int[linha][coluna];
-
+        // preencher a matriz de forma random
         for (int i = 0; i < linha; i++) {
             for (int j = 0; j < coluna; j++) {
                 matriz[i][j] = rd.nextInt(10);
@@ -23,7 +26,7 @@ public class Formativa {
 
         }
         System.out.println("Valores do sistema:");
-
+        // substituir valores pela regra de preenchimento
         for (int i = 0; i < linha; i++) {
             for (int j = 0; j < coluna; j++) {
                 matriz[i][j] = rd.nextInt(10);
@@ -50,14 +53,15 @@ public class Formativa {
         int nSorteado = rd.nextInt(1000);
         int nDigitado = 0;
         int contador = 1;
-
+        // laço não contavél (quando não sei a quantidade - while)
         while (nSorteado != nDigitado) {
             System.out.println("Tente adivinhar o número sorteado entre 0 e 1000");
             nDigitado = sc.nextInt();
             if (nSorteado > nDigitado) {
                 System.out.println("O número digitado é menor do que o número sorteado, tente outra vez");
                 contador++;
-            } else if (nSorteado < nDigitado) {
+            }
+            if (nSorteado < nDigitado) {
                 System.out.println("O número digitado é maior do que o número sorteado, tente outra vez");
                 contador++;
             }
@@ -66,5 +70,34 @@ public class Formativa {
         System.out.println("Parabéns, você acertou em " + contador + " tentativa");
     }
 
-
+    public void exercicio03() {
+        // criar um vetor 100-1000 itens
+        int vetor[] = new int[rd.nextInt(900) + 100];
+        // preencher o vetor
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = rd.nextInt(100) + 1;
+        }
+        // procurar os nº Impares
+        int contImparNaPar = 0;
+        int contParNaImpar = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] % 2 == 1) {// resto == 1
+                System.out.println("casa impar " + vetor[i]);
+                // se esta na casa par
+                if (i % 2 == 1) {// casa par
+                    contImparNaPar++;
+                }
+            }
+        }
+        // procurar os nº Pares
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] % 2 == 0) {// resto == 0
+                System.out.println("casa par " + vetor[i]);
+                 // se esta na casa impar
+                 if (i % 2 == 0) {// casa impar
+                    contParNaImpar++;
+                }
+            }
+        }
+    }
 }
